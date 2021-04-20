89c6ec20aa443792018983e1aeaccbeb023fd6c2
Saara <astonedf@users.noreply.github.com>
2021-04-20 16:08:04 +0200

    line charts

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
+// set the dimensions and margins of the graph
+var margin = {top:20, right:20, bottom:30, left:50},
+    width = 4960 - margin.left - margin.right, height = 500 - margin.top - margin.bottom;
+
+
+// set the ranges
+var x = d3.scaleBand().range([0,width]);
+var y = d3.scaleLinear().range([height,0]);
+
+// define the line
+var valueline = d3
+    .line()
+    .x(function(d) {return x(d.name); })
+    .y(function(d) {return y(d.att); });
+
+var valueline2 = d3
+    .line()
+    .x(function(d) {return x(d.name); })
+    .y(function(d) {return y(d.def); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'// moves the 'group' element to the top left margin
+var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g").attr("transform","translate("+ margin.left +","+ margin.top +")");
+
+// Get the data
+d3.csv("riot_champion.csv").then(function(data) {
+    // format the data
+    data.forEach(function(d) {
+        d.name = d.name
+        d.att = +d["stats.attackdamage"]
+        d.def = +d["stats.armor"]
+    });
+
+    // Scale the range of the data
+    x.domain(data.map(function(d) {return d.name; }));
+    y.domain([0, d3.max(data,function(d) {return d.att; })]);
+
+    // Add the valueline 
+    svg.append("path")
+        .data([data])
+        .attr("class","line")
+        .attr("d", valueline);
+    
+    svg.append("path")
+        .data([data])
+        .attr("class","line")
+        .style("stroke","red")
+        .attr("d", valueline2);
+
+// Add the X Axis
+    svg.append("g")
+        .attr("transform","translate(0,"+ height +")")
+        .call(d3.axisBottom(x));
+        
+// Add the Y Axis
+    svg.append("g").call(d3.axisLeft(y));
+});
+
+
+/*
 let mp = []
 
-FileAttachment("riot_champion.csv")
+
 
 lol_data = d3.csv("riot_champion.csv", function(data){
     return {
@@ -8,19 +73,17 @@ lol_data = d3.csv("riot_champion.csv", function(data){
         tags : data.tags,
         key : +data.key
     }
-});
-
-
-d3.csv("riot_champion.csv").then(function appendData(data){
+}).then(data => {
     for (var i = 1; i < data.length; i++) {
         value = data[i]['stats.mp']
         //console.log(data[i]['name'])
         mp.push(parseInt(value))
-        
+    
     }
+    console.log("data", data)
 })
 
-console.log(lol_data)
+console.log("promise",lol_data)
 
 let corps = d3.select('body');
 
@@ -37,22 +100,14 @@ async function create_circles(){
     setTimeout(()=>{
         d3.select(".chart")
             .selectAll("div")
-            .data(mp)
+            .data(lol_data)
                 .enter()
                 .append("div")
-                .style("width", function(d) { return d + "px"; })
-                .text(function(d) { return d; });
-        console.log("inside timeout");
-        /* canvas.selectAll('circle')
-        .data(mp)
-        .enter()
-        .append('circle')
-            .attr('cx',d => d)
-            .attr('cy',300)
-            .attr('r',30)
-            .style('opacity',0.5); */
+                .style("width", function(d) { return d.keys + "px"; })
+                .text(function(d) { return d.name; });
+        console.log("inside timeout"); 
     },5000);
 
 }
 
-create_circles();
+create_circles(); */
---
+++ testfile.js
@@ -0,0 +1,68 @@
+let mp = []
+
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+d3.csv("riot_champion.csv",function(d){
+    // fonction de conversion
+    // second paramètre
+    return {
+      // 2,3,4 ne conserver que trois données
+      // doubler la valeur de terrain au passage
+      name : d.name,
+      key : +d.key, 
+      
+    }
+  }).then(data => {
+    // affichage des données
+    console.log("Data",data);
+
+  // X axis: scale and draw:
+  var x = d3.scaleBand()
+      .domain([data, d => d.name])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, width]);
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // set the parameters for the histogram
+  var histogram = d3.bin()
+      .value(d => d.key)   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(70)); // then the numbers of bins
+
+  // And apply this function to data to get the bins
+  var bins = histogram(data);
+
+  console.log(bins)
+
+  // Y axis: scale and draw:
+  var y = d3.scaleLinear()
+      .range([height, 0]);
+      y.domain([0, d3.max(bins, d => d.length)]);   // d3.hist has to be called before the Y axis obviously
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  // append the bar rectangles to the svg element
+  svg.selectAll("rect")
+      .data(bins)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#69b3a2")
+
+});
